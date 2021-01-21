4053b573cfde17011f6c0c24da9c2ddd2eb56697
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-01-21 23:31:26 +0700

    File Main.kt corrected

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
-// For cards VisaMir & MasterMaestro
+// For cards Visa / MIR / MasterCard / Maestro
 const val MONTH_LIMIT_CARD = 60_000_000
 const val DAY_LIMIT_CARD = 15_000_000
-// For VIsaMir
+const val EXCEED_LIMIT = -1.0
+const val INVALID_CARD_ACCOUNT = -2.0
+// For VIsa & MIR
 const val TRANSFER_FEE_VISA_MIR = 0.0075
 const val MINIMUM_COMISSION_VISA_MIR = 3_500.00
-// For MasterMaestro
+// For MasterCard & Maestro
 const val TRANSFER_FEE_MC_ME = 0.006
-const val PERMANENT_COMISSION_VISA_MIR = 2_000.00
+const val PERMANENT_COMISSION_MC_ME = 2_000.00
 const val ONCE_LIMIT_CARD = 7_500_000
 // For VKPay
 const val TRANSFER_FEE_VKPAY = 0.0
@@ -16,35 +18,49 @@ const val DAY_LIMIT_ACC = 1_500_000
 fun main(args: Array<String>) {
     val amountFee = calcFee(amount = 8_000_000)
     val fee = Math.round(amountFee).toInt()
-    when(fee) {
-        -1, -2, -3 -> println("\n\t\tОперация невозможна. \nПричина - превышение лимита (дневного и/или месячного). " +
-                "\nПопробуйте уменьшить сумму транзакции.")
-        -4 -> println("\n\tОперация невозможна - указана неверная карта или счет.")
-        else -> println("\n\tКомиссия к оплате: ${fee / 100}руб. ${fee % 100}коп.")
+    printResults(fee)
+}
+
+fun calcFee(nameCardAccount: String = "MasterCard", monthAmount: Int = 0, amount: Int): Double {
+    return when (nameCardAccount) {
+        "Visa" -> calcVisaMir(monthAmount, amount)
+        "MIR" -> calcVisaMir(monthAmount, amount)
+        "MasterCard" -> calcVisaMir(monthAmount, amount)
+        "Maestro" -> calcVisaMir(monthAmount, amount)
+        "VKPay" -> calcVkPay(monthAmount, amount)
+        else -> INVALID_CARD_ACCOUNT
     }
 }
-// nameCardAccount == VKPay / VisaMir / MasterMaestro
-fun calcFee(nameCardAccount: String = "VKPay", monthAmount: Int = 0, amount: Int): Double {
-    return if(nameCardAccount == "VisaMir") {
-        if(monthAmount + amount > MONTH_LIMIT_CARD || amount > DAY_LIMIT_CARD) {
-            -1.0
-        } else {
-            if(amount * TRANSFER_FEE_VISA_MIR <= MINIMUM_COMISSION_VISA_MIR) MINIMUM_COMISSION_VISA_MIR
-            else amount * TRANSFER_FEE_VISA_MIR
-        }
-    } else if (nameCardAccount == "MasterMaestro") {
-        if(monthAmount + amount > MONTH_LIMIT_CARD || amount > DAY_LIMIT_CARD) {
-            -2.0
-        } else {
-            if(amount + monthAmount <= ONCE_LIMIT_CARD) 0.0 else amount * TRANSFER_FEE_MC_ME + PERMANENT_COMISSION_VISA_MIR
-        }
-    } else if (nameCardAccount == "VKPay") {
-        if(monthAmount + amount > MONTH_LIMIT_ACC || amount > DAY_LIMIT_ACC) {
-            -3.0
-        } else {
-            amount * TRANSFER_FEE_VKPAY
-        }
+
+fun calcVisaMir(monthAmount: Int = 0, amount: Int): Double {
+    return if(monthAmount + amount > MONTH_LIMIT_CARD || amount > DAY_LIMIT_CARD) {
+        EXCEED_LIMIT
     } else {
-        -4.0
+        if(amount * TRANSFER_FEE_VISA_MIR <= MINIMUM_COMISSION_VISA_MIR) MINIMUM_COMISSION_VISA_MIR else amount * TRANSFER_FEE_VISA_MIR
+    }
+}
+
+fun calcMasterMaestro(monthAmount: Int = 0, amount: Int): Double {
+    return if(monthAmount + amount > MONTH_LIMIT_CARD || amount > DAY_LIMIT_CARD) {
+        EXCEED_LIMIT
+    } else {
+        if(amount + monthAmount <= ONCE_LIMIT_CARD) 0.0 else amount * TRANSFER_FEE_MC_ME + PERMANENT_COMISSION_MC_ME
+    }
+}
+
+fun calcVkPay(monthAmount: Int = 0, amount: Int): Double {
+    return if(monthAmount + amount > MONTH_LIMIT_ACC || amount > DAY_LIMIT_ACC) {
+        EXCEED_LIMIT
+    } else {
+        amount * TRANSFER_FEE_VKPAY
+    }
+}
+
+fun printResults(fee: Int) {
+    when(fee) {
+        EXCEED_LIMIT.toInt() -> println("\n\t\tОперация невозможна. \nПричина - превышение лимита (дневного и/или месячного). " +
+                "\nПопробуйте уменьшить сумму транзакции.")
+        INVALID_CARD_ACCOUNT.toInt() -> println("\n\tОперация невозможна - указана неверная карта или счет.")
+        else -> println("\n\tКомиссия к оплате: ${fee / 100}руб. ${fee % 100}коп.")
     }
 }
